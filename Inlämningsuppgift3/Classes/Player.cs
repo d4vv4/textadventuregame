4343ae10db3f00e4b4f30e8199f99538011efbc9
David Johansson <snubbla@gmail.com>
2020-10-06 12:14:43 +0200

    Add project files.

---
+++ Inlämningsuppgift3/Classes/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift3.Classes
+{
+    public class Player
+    {
+        public List<Item> _Items { get; set; }
+        public Room _Location { get; set; }
+    }
+}
---
+++ Inlämningsuppgift3/Classes/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift3.Classes
+{
+    public class Room
+    {
+        public string _Name { get; set; }
+        public List<string> _ { get; set; }
+
+        public Room()
+        {
+            GenerateRoom();
+        }
+
+        public Room GenerateRoom()
+        {
+            Random rand = new Random();
+        }
+    }
+}
